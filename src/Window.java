9b2f2fe2dc3757859ffc21ba947142699bb1af0b
Mattias Johansson <mattias.johansson@atex.com>
2014-03-02 23:14:52 +0100

    Added lib and a Window-class

--- src/Window.java
+++ src/Window.java
@@ -1,8 +1,25 @@
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
 
 public class Window {
 
-    public static void main(String[] args) {
-        System.out.println("LOL");
+    public static void main(String[] argv) throws Exception {
+        Display.setDisplayMode(new DisplayMode(800, 600));
+        Display.create();
+        setup();
+        while (!Display.isCloseRequested()) {
+            render();
+            Display.update();
+        }
+        Display.destroy();
     }
 
+    protected static void setup() {
+    }
+
+    protected static void render() {
+        glClear(GL_COLOR_BUFFER_BIT);
+    }
 }
