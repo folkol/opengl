b1518d0c23e49553534887a651ceb3a17894c2d1
Mattias Johansson <mattias.johansson@atex.com>
2014-03-22 23:21:52 +0100

    Now rotating around Y instead

--- resources/vertex.glsl
+++ resources/vertex.glsl
@@ -8,10 +8,15 @@ uniform vec2 resolution;
 
 void main(void) {
     float Angle = 2. * mouse.x/resolution.x;
-    mat4 rotz = mat4(cos( Angle ), -sin( Angle ), 0.0, 0.0,
-                    sin( Angle ),  cos( Angle ), 0.0, 0.0,
-                    0.0,           0.0,          1.0, 0.0,
-                    0.0,           0.0,          0.0, 1.0 );
+//    mat4 rotz = mat4(cos( Angle ), -sin( Angle ), 0.0, 0.0,
+//                    sin( Angle ),  cos( Angle ),  0.0, 0.0,
+//                    0.0,           0.0,           1.0, 0.0,
+//                    0.0,           0.0,           0.0, 1.0);
+
+    mat4 rotz = mat4(cos( Angle ),  0.0, sin( Angle ), 0.0,
+                     0.0,           1.0, 0.0,          0.0,
+                     -sin( Angle ), 0.0, cos( Angle ), 0.0,
+                     0.0,           0.0, 0.0,          1.0);
 
     gl_Position = rotz * in_Position;
 }
