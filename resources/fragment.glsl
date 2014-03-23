261ed6a49f9f757bf61167a6b5d58a60f2073e57
Folkol <mattias4@kth.se>
2014-03-23 12:17:02 +0100

    Minor fixes

--- resources/fragment.glsl
+++ resources/fragment.glsl
@@ -7,16 +7,16 @@ uniform vec2 mouse;
 uniform vec2 resolution;
 
 #define SPEED 10.0
-#define WAVELENGTH 100 // Higher number = shorter wavelength :P
+#define WAVELENGTH 0.1 // Higher number = shorter wavelength :P
 #define AMPLITUDE 0.2
 
 float wave(vec2 origin)
 {
-  float r = distance(gl_FragCoord.xy/resolution, origin/resolution);
+  float r = distance(gl_FragCoord.xy, origin);
   return 0.5 + AMPLITUDE * sin(-SPEED * time + WAVELENGTH * r);
 }
 
 void main( void ) {
-  float color = 0.5 * wave(mouse) + wave(resolution - mouse);
-  out_Color = vec4( color / 2., 0., 0., 1);
+  float color = 0.5 * (wave(mouse) + wave(resolution - mouse));
+  out_Color = vec4( vec3(color), 1);
 }
